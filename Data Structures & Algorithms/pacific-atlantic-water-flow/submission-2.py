b1b2423a4d5f57eea79d29c717c7a4462997a725
Rrutum Lavana <67268134+rrutum42@users.noreply.github.com>
2026-08-31 12:46:43 +0530

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # O(m*n), O(m*n)
+        ans = []
+
+        pac,atl = set(),set()
+        rows = len(heights)
+        cols = len(heights[0])
+
+        def dfs(r,c,visited,prevHeight):
+            if(
+                r<0 or c<0 or
+                r>=rows or c>=cols or
+                heights[r][c] < prevHeight or 
+                (r,c) in visited
+            ):
+                return
+            
+            visited.add((r,c))
+            currHeight = heights[r][c]
+            dfs(r+1,c,visited,currHeight)
+            dfs(r-1,c,visited,currHeight)
+            dfs(r,c+1,visited,currHeight)
+            dfs(r,c-1,visited,currHeight)
+
+        for r in range(rows):
+            # first col for pacific
+            dfs(r,0,pac,heights[r][0])
+            # last col for atlantic
+            dfs(r,cols-1,atl,heights[r][cols-1])
+        
+        for c in range(cols):
+            # FIRST ROW FOR PACIFIC
+            dfs(0,c,pac,heights[0][c])
+            # last row for atlantic
+            dfs(rows-1,c,atl,heights[rows-1][c])
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    ans.append([r,c])
+        
+        return ans
+        
