9084ca9a2495fec79bb1ae97db55b9e24d1e6212
Rrutum Lavana <67268134+rrutum42@users.noreply.github.com>
2026-08-31 14:34:46 +0530

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        '''
+        as curr str as centre calc len of palindrome
+        O(n^2), O(n)
+        '''
+        start = 0
+        maxLen = 0
+
+        def expand_around_centre(l,r):
+            nonlocal start,maxLen
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                currLen =  r - l + 1
+                if currLen > maxLen:
+                    maxLen = currLen
+                    start = l
+                l -= 1
+                r += 1
+
+        for i in range(len(s)):
+            # odd
+            expand_around_centre(i,i)
+            # even
+            expand_around_centre(i,i+1)
+
+        return s[start:start+maxLen]
